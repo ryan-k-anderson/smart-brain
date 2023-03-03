2ed60b257051705c51a5960bf38ab5910412ed2b
Ryan Kristopher Anderson <78626914+ryan-k-anderson@users.noreply.github.com>
2023-03-03 10:07:06 -0500

    Built basic looks to application

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Tilt from 'react-parallax-tilt';
+import './Logo.css'
+import brain from "./brain.png"
 
 const Logo = ()=>{
     return(
-        <div className='ma4' style={{maxWidth: '300px'}}>
-            <Tilt width='300px'>
-                <div style={{ height: '300px', width:'300px', backgroundColor: 'darkgreen' }}>
-                    <h1>React Parallax Tilt 👀</h1>
+        <div className='ma4' style={{maxWidth: '200px'}}>
+            <Tilt className='Tilt' width='200px'>
+                <div style={{ height: '200px', width:'200px', padding: '3px'}}>
+                    <img style={{paddingTop: '5px', height: '200px'}} src={ brain } alt='brain-logo'></img>
                 </div>
                 </Tilt>
         </div>
