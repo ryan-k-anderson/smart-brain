6e892faa7df41cc79ccb1d0d4a81f555b1fa6dc5
Ryan Kristopher Anderson <78626914+ryan-k-anderson@users.noreply.github.com>
2023-03-03 13:09:02 -0500

    Started Functionality of button and input field

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
+import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import Rank from './components/Rank/Rank'
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value)
+  }
+
+  onButtonSubmit = () => {
+    console.log('Click')
+  }
+  render() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm 
+      onInputChange={this.onInputChange} 
+      onButtonSubmit={this.onButtonSubmit} />
       {/* {<FaceRecognition />} */}
     </div>
   );
+  }
 }
 
 export default App;
